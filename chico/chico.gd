2634afab0e079aebd90894a0333e3a057de20cf5
vlcanesin <vlcanesin@gmail.com>
2024-03-25 23:02:43 -0300

    Implementação inicial do diálogo

--- chico/chico.gd
+++ chico/chico.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 const SPEED = 200.0
 
 @onready var _walk_sprite = $WalkSprite
+@onready var actionable_finder: Area2D = $ActionableFinder
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("ui_accept"):
-		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue_text.dialogue"), "start")
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
