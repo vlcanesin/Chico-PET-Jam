f3f8bbcc02a19c4f5059d41ee0009416f7916709
vlcanesin <vlcanesin@gmail.com>
2024-03-29 14:36:06 -0300

    Implementação do conteúdo do balão de pensamento

--- chico/chico.gd
+++ chico/chico.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 const SPEED = 200.0
 var orientation: String = "right"
 var has_actionable_collision: bool = false
+var thought: String = ""
 
 @onready var _walk_sprite = $WalkSprite
-@onready var _thought_sprite = $Thought
+@onready var _thought_node = $Thought
+@onready var _balloon_sprite = $Thought/Balloon
+@onready var _content_sprite = $Thought/Content
 @onready var actionable_finder: Area2D = $ActionableFinder
 
 func _unhandled_input(event):
@@ -16,6 +19,10 @@ func _unhandled_input(event):
 			return
 
 func _process(delta):
+	_process_movement(delta)
+	_process_thought()
+		
+func _process_movement(delta) -> void:
 	if Input.is_action_pressed("ui_right"):
 		orientation = "right"
 		_walk_sprite.play("right")
@@ -26,21 +33,37 @@ func _process(delta):
 		position.x -= SPEED*delta
 	else:
 		_walk_sprite.stop()
-		
+
+func _process_thought() -> void:
 	var actionables = actionable_finder.get_overlapping_areas()
 	if actionables.size() > 0:
+		thought = actionables[0].get_trigger_thought()
 		has_actionable_collision = true
 	else:
 		has_actionable_collision = false
 	
 	if has_actionable_collision:
 		if orientation == "right":
-			_thought_sprite.offset.x = 350
-			_thought_sprite.play("right")
+			_thought_node.position.x = 120
+			_balloon_sprite.play("right")
+			_play_thought_content()
 		else:
-			_thought_sprite.offset.x = -350
-			_thought_sprite.play("left")
-		_thought_sprite.visible = true
+			_thought_node.position.x = -120
+			_balloon_sprite.play("left")
+			_play_thought_content()
+		_thought_node.visible = true
 	else:
-		_thought_sprite.stop()
-		_thought_sprite.visible = false
+		_balloon_sprite.stop()
+		_content_sprite.stop()
+		_thought_node.visible = false
+
+func _play_thought_content() -> void:
+	match thought:
+		"escutar":
+			if orientation == "right":
+				_content_sprite.position.x = 6
+			else:
+				_content_sprite.position.x = -6
+			_content_sprite.play("escutar")
+		_:
+			assert(false)  # throws error
--- common/actionable.gd
+++ common/actionable.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
+@export var trigger_thought: String = ""
 
 func action() -> void:
 	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+
+func get_trigger_thought() -> String:
+	return trigger_thought
