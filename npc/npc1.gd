13aa8112682c00673890fad93733ede4b78a34df
vlcanesin <vlcanesin@gmail.com>
2024-03-30 11:14:33 -0300

    Commit para push

--- npc/npc1.gd
+++ npc/npc1.gd
@@ -1,20 +1,18 @@
 extends Node2D
 
 @onready var _sprite = $AnimatedSprite2D
-@onready var _dialogue_manager = $DialogueManager
 var _sprite_state: String = "idle"
 
 func _ready():
-	_dialogue_manager.start_dialogue.connect(_on_start_dialogue)
-	_dialogue_manager.end_dialogue.connect(_on_end_dialogue)
+	pass
 
 func _process(_delta):
 	_sprite.play(_sprite_state)
 
-func _on_start_dialogue():
-	print("Entrei no diálogo")
+func _on_dialogue_manager_start_dialogue():
+	#print("Entrei no diálogo")
 	_sprite_state = "speak"
 
-func _on_end_dialogue():
-	print("Saí no diálogo")
+func _on_dialogue_manager_end_dialogue():
+	#print("Saí no diálogo")
 	_sprite_state = "idle"
