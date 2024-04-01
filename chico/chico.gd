4b5117518588b300f4dd108258a562fb8c80b52e
vlcanesin <vlcanesin@gmail.com>
2024-03-31 22:24:50 -0300

    Shh!! Chico está dormindo!

--- chico/chico.gd
+++ chico/chico.gd
@@ -5,6 +5,7 @@ const SLEEP_TIMEOUT: float = 10.0
 var default_height: int = 438  # OBS: não está como constante pois pode variar de cena pra cena
 var orientation: String = "right"
 var thought: String = ""
+var is_sleeping: bool = true
 
 @export var SPEED : float = 300.0
 
@@ -22,6 +23,13 @@ func _unhandled_input(_event):
 		if actionables.size() > 0:
 			actionables[0].action()
 			return
+	if Input.is_key_pressed(KEY_M):
+		print("Mijar")
+	if Input.is_key_pressed(KEY_L):
+		print("Latir")
+
+func _ready():
+	sleep_timer.start(0)  # Começa dormindo
 
 func _process(delta):
 	if not DialogueGlobals.in_dialogue:
@@ -34,19 +42,24 @@ func _process(delta):
 
 func _reset_timer(time: float = SLEEP_TIMEOUT):
 	sleep_timer.start(time)
+	#print("Timer set to:", time)
 	
 func _process_movement(delta) -> void:
 	if Input.is_action_pressed("ui_right"):
+		is_sleeping = false
 		_reset_timer()
 		orientation = "right"
+		_walk_sprite.flip_h = false
 		_walk_sprite.play("right")
 		position.x += SPEED*delta
 	elif Input.is_action_pressed("ui_left"):
+		is_sleeping = false
 		_reset_timer()
 		orientation = "left"
+		_walk_sprite.flip_h = false
 		_walk_sprite.play("left")
 		position.x -= SPEED*delta
-	else:
+	elif not is_sleeping:
 		_walk_sprite.stop()
 
 func _process_height():
@@ -86,4 +99,9 @@ func _play_thought_content() -> void:
 	_content_sprite.play(thought)
 
 func _on_sleep_timer_timeout():
+	is_sleeping = true
+	if orientation == "left":
+		_walk_sprite.flip_h = true
+	else:
+		_walk_sprite.flip_h = false
 	_walk_sprite.play("sleep")
