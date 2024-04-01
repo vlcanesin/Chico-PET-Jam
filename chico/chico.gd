cb4bcc719a021b7bb76ad39feeb9f21acdec97c6
vlcanesin <vlcanesin@gmail.com>
2024-03-31 22:52:00 -0300

    MIJAR

--- chico/chico.gd
+++ chico/chico.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 const SLEEP_TIMEOUT: float = 10.0 
+const MIJO_LENGTH: float = float(8)/12
 
 var default_height: int = 438  # OBS: não está como constante pois pode variar de cena pra cena
 var orientation: String = "right"
 var thought: String = ""
 var is_sleeping: bool = true
+var is_MIJANDO: bool = false
 
 @export var SPEED : float = 300.0
 
@@ -16,6 +18,7 @@ var is_sleeping: bool = true
 @onready var actionable_finder: Area2D = $ActionableFinder
 @onready var height_finder: Area2D = $HeightFinder
 @onready var sleep_timer: Timer = $SleepTimer
+@onready var MIJO_timer: Timer = $MIJOTimer
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -23,8 +26,12 @@ func _unhandled_input(_event):
 		if actionables.size() > 0:
 			actionables[0].action()
 			return
-	if Input.is_key_pressed(KEY_M):
-		print("Mijar")
+	if Input.is_key_pressed(KEY_M) and not is_sleeping:
+		is_sleeping = false
+		is_MIJANDO = true
+		_fix_orientation()
+		_walk_sprite.play("MIJAR")
+		_reset_MIJO_timer()
 	if Input.is_key_pressed(KEY_L):
 		print("Latir")
 
@@ -38,28 +45,37 @@ func _process(delta):
 		_process_thought()
 	else:
 		_thought_node.visible = false
-		_reset_timer()
+		_reset_sleep_timer()
 
-func _reset_timer(time: float = SLEEP_TIMEOUT):
+func _reset_MIJO_timer(time: float = MIJO_LENGTH):
+	MIJO_timer.start(time)
+
+func _reset_sleep_timer(time: float = SLEEP_TIMEOUT):
 	sleep_timer.start(time)
 	#print("Timer set to:", time)
 	
+func _fix_orientation():
+	if orientation == "left":
+		_walk_sprite.flip_h = true
+	else:
+		_walk_sprite.flip_h = false
+
 func _process_movement(delta) -> void:
 	if Input.is_action_pressed("ui_right"):
 		is_sleeping = false
-		_reset_timer()
+		_reset_sleep_timer()
 		orientation = "right"
 		_walk_sprite.flip_h = false
 		_walk_sprite.play("right")
 		position.x += SPEED*delta
 	elif Input.is_action_pressed("ui_left"):
 		is_sleeping = false
-		_reset_timer()
+		_reset_sleep_timer()
 		orientation = "left"
 		_walk_sprite.flip_h = false
 		_walk_sprite.play("left")
 		position.x -= SPEED*delta
-	elif not is_sleeping:
+	elif not is_sleeping and not is_MIJANDO:
 		_walk_sprite.stop()
 
 func _process_height():
@@ -100,8 +116,11 @@ func _play_thought_content() -> void:
 
 func _on_sleep_timer_timeout():
 	is_sleeping = true
-	if orientation == "left":
-		_walk_sprite.flip_h = true
-	else:
-		_walk_sprite.flip_h = false
+	_fix_orientation()
 	_walk_sprite.play("sleep")
+
+func _on_MIJO_timer_timeout():
+	print("asdasd")
+	MIJO_timer.stop()
+	_walk_sprite.stop()
+	is_MIJANDO = false
