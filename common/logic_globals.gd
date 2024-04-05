5f074c7711398ed6e3be5eb41e3b62d9ccd45679
zukovCarlos <zukovcarlos@gmail.com>
2024-04-04 22:37:45 -0300

    implementacao da quest do lapis
    falta só colocar o lapis pra baixo

--- common/logic_globals.gd
+++ common/logic_globals.gd
@@ -8,6 +8,6 @@ var unlock_tri: bool = false
 var enable_collision_actionables: bool = true
 
 var chico_start_position: Vector2 = Vector2(4303, 435)
-var chico_speed: float = 300.0
+var chico_speed: float = 3000.0
 var chico_already_seen_sagui: bool = false
 var chico_already_ALGOAQUI: bool = false
--- inventory/Collectable.gd
+++ inventory/Collectable.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @export var itemRes: InventoryItem
 
+func _process(_delta):
+	if itemRes.name == "Lapis":
+		if LogicGlobals.unlock_calc_pencil:
+			self.position.y = 20
+
 func collect(inventory: Inventory):
 	inventory.insert(itemRes)
 	queue_free()
